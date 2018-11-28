88b96b6075c79be8db47cdc5d9330da60da5c3a4
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2018-11-28 17:50:39 -0500

    unbreak other controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  before_action :fetch_domain_mapping
+
+
+  def fetch_domain_mapping
+    @mapping = DomainMapping.find_by_host(request.host)
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-    @mapping = DomainMapping.find_by_host(request.host)
-
     file = File.open("db/seeds_authors_books.rb", "rb")
     @seed_file = file.read
     file.close
